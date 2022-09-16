a97537fa01d0f62abca2314ccb1179f0fd372dcd
RevinFedor <fr75793@gmail.com>
2022-09-17 04:54:33 +0500

    two section

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const gif = document.querySelector(".scope_gif_img");
+const gif_block = document.querySelector(".scope_gif_block");
+const content = document.querySelector(".scope");
+
+document.onscroll = function () {
+  if (
+    document.body.scrollTop > 200 
+  ) {
+    console.log("hello");
+  } 
+};
+
+
+
+window.addEventListener("scroll", function () {
+    let scrollTop = window.scrollY
+    let scrollSection = content.getBoundingClientRect().top;
+    let scrollSectionHeight = content.offsetHeight;
+    // Если экран перед блоком
+    if (scrollSection > 0 ) {
+      gif.classList.remove("fixed");
+      gif_block.style.bottom = '';
+    } 
+    // если экран достигает блока
+    if (scrollSection < 0) {
+      gif.classList.add("fixed");
+    } 
+    // если экран выходит за блок
+    if (scrollSection < -scrollSectionHeight + 700) {
+      gif_block.style.bottom = 0;
+      gif.classList.remove("fixed");
+    }
+});
